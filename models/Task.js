53db620c401ab0709e02a17c0c2ae47b977bfedf
RamDev <daudalaramadan2@gmail.com>
2025-03-21 14:53:29 +0000

    ✨ add Task model schema for MongoDB using Mongoose

---
+++ models/Task.js
@@ -0,0 +1,7 @@
+const mongoose = require("mongoose");
+// A model is a JavaScript class that represents a collection in MongoDB.
+
+// A Mongoose model provides an interface to the database for creating, querying, updating, deleting records, etc.
+const TaskSchema = new mongoose.Schema({
+  // 1. we define the schema here
+});
