aa4bd43abfa83275a952d796b1b11c7df219fc0f
RamDev <daudalaramadan2@gmail.com>
2025-03-22 12:23:16 +0000

    ✨ add name and completed fields to Task model schema

--- models/Task.js
+++ models/Task.js
@@ -4,4 +4,14 @@ const mongoose = require("mongoose");
 // A Mongoose model provides an interface to the database for creating, querying, updating, deleting records, etc.
 const TaskSchema = new mongoose.Schema({
   // 1. we define the schema here
+  name: {
+    type: String,
+    required: [true, "Please provide a name"],
+    trim: true,
+    maxlength: [20, "Name can not be more than 20 characters"],
+  },
+  completed: {
+    type: Boolean,
+    default: false,
+  },
 });
